2a4cf06214d67031bbc520507e87d886b8971a19
Maruf2662 <gwithgamerz@gmail.com>
2025-04-03 01:05:40 +0200

    Update config.js

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ global.botnumber = "" //Inserisci nelle apici il numero del bot (es. +3937620304
 global.confirmCode = ""
 
 global.owner = [
-    ['393508416499', 'Proprietario', true],
+    ['393246014915', 'Proprietario', true],
 ] //Numero degli owner
 
 //────────────────────────────
